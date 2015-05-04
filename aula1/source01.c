60921f850c29b884d71bf1f9f1da0ec4ad58be1f
rscampos <raphaelcampos.rp@gmail.com>
2015-05-03 22:04:31 -0300

    Source code 01

---
+++ aula1/source01.c
@@ -0,0 +1,11 @@
+/* Source code for the 'debugging class'.
+ * source01.c - compiling with/without debugger symbols
+ * Raphael Campos Silva <raphaelcampos.rp@gmail.com>
+ * 23/04/2015
+ */
+
+#include<stdio.h>
+
+void main(int argc, char ** argv){
+        printf("Hello Debbuging Class\n");
+}
