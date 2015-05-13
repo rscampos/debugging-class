6c3a6a8716e7005bfd7da40bbe5fd062dc57f9a6
Raphael <raphaelcampos.rp@gmail.com>
2015-05-13 16:31:13 -0300

    aula 2

---
+++ aula2/source03.c
@@ -0,0 +1,20 @@
+/* Source code for the 'debugging class'. 
+ * source03.c - working with breakpoints.
+ * Raphael Campos Silva <raphaelcampos.rp@gmail.com>
+ * 13/05/2015
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void echoinput(char * string){
+	char buffer[30];
+	strcpy(buffer, string);
+	printf("String: %s\n", buffer);
+
+}
+
+void main(int argc, char ** argv){
+	echoinput(argv[1]);
+	exit(0);
+}
